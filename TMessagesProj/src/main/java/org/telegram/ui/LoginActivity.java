a6601fbb6e7d59c85655f037f351db7227fe8827
vinayganti001 <vinay.ganti001@gmail.com>
2026-02-24 06:19:58 +0000

    Hack but working demo for mwc - initial

--- TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
@@ -3575,7 +3575,17 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
                     return;
                 }
                 if (authObject instanceof TLRPC.TL_auth_authorization) {
-                    onAuthSuccess((TLRPC.TL_auth_authorization) authObject);
+                    onAuthSuccess((TLRPC.TL_auth_authorization) authObject, true);
+                } else if (authObject instanceof TLRPC.TL_auth_authorizationSignUpRequired) {
+                    AndroidUtilities.runOnUIThread(() -> {
+                         String requestPhone = PhoneFormat.stripExceptNumbers("" + codeField.getText() + phoneField.getText());
+                         String requestPhoneHash = "";
+                         Bundle bundle = new Bundle();
+                         bundle.putString("phoneFormated", requestPhone);
+                         bundle.putString("phoneHash", requestPhoneHash);
+                         bundle.putString("code", codeField.getText().toString());
+                         setPage(VIEW_REGISTER, true, bundle, false);
+                    });
                 }
             });
         }
